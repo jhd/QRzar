62b6f051a62d767c50ae3424f901156fe30fca20
jonathan <jhennes@tcd.ie>
2012-02-08 22:09:37 +0000

    Added dummy authentication variable for Facebook login

--- qrzar/src/com/javazombies/qrzar/Player.java
+++ qrzar/src/com/javazombies/qrzar/Player.java
@@ -3,12 +3,18 @@ package com.javazombies.qrzar;
 public class Player {
 	
 	private boolean loggedIn;
+	private int Fauth; /* Facebook authentication token */
 	
 	public Player(){
+		/* TODO read from file from previous login */
 		loggedIn = false;
 	}
 	
 	public boolean loggedIn(){
 		return loggedIn;
+	}
+	
+	public int Fauth(){
+		return Fauth;
 	}
 }
