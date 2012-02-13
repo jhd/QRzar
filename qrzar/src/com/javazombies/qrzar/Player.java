b184e9cf7ed38f86001d13025c5ff71666474eed
jonathan <jhennes@tcd.ie>
2012-02-13 23:47:23 +0000

    Added some methods for Player

--- qrzar/src/com/javazombies/qrzar/Player.java
+++ qrzar/src/com/javazombies/qrzar/Player.java
@@ -3,6 +3,7 @@ package com.javazombies.qrzar;
 public class Player {
 	
 	private boolean loggedIn;
+	private Player target;
 	
 	public Player(){
 		loggedIn = false;
@@ -16,4 +17,17 @@ public class Player {
 		loggedIn = value;
 	}
 	
+	public String[] getGames(){
+		/*
+		 * Return String[] of the player's current games
+		 */
+		return new String[1];		
+	}
+	
+	public Player getTarget(){
+		/*
+		 * Return a Player class of the local player's current target
+		 */
+		return new Player();
+	}
 }
